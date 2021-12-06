0137af9dc9dce78c56e75ce01b9a2944c5f41a8f
Mohamed Amine Berroug <ma.berroug@aggrom.com>
2021-12-06 15:28:54 +0100

    dev and update event collection

--- api/eventserviceprovider/controllers/eventserviceprovider.js
+++ api/eventserviceprovider/controllers/eventserviceprovider.js
@@ -6,6 +6,119 @@ const { sanitizeEntity } = require('strapi-utils');
  */
 
 module.exports = {
+    async getListOfHosting(ctx) {
+        let entities;
+        const { city } = ctx.params;
+        if (ctx.query._q) {
+            entities = await strapi.services.eventserviceprovider.search(ctx.query);
+        } else {
+            entities = await strapi.services.eventserviceprovider.find(ctx.query);
+        }
+        let eventserviceproviders = entities.map(entity => sanitizeEntity(entity, { model: strapi.models.eventserviceprovider }));
+        let myItems = []
+        for (let i = 0; i < eventserviceproviders.length; i++) {
+
+            if ((eventserviceproviders[i].address.city == city) && (eventserviceproviders[i].status)) {
+                for (let d = 0; d < eventserviceproviders[i].showIn.length; d++) {
+                    if (eventserviceproviders[i].showIn[d].serviceName == "event-hosting") {
+                        let newItem = {
+                            id: eventserviceproviders[i].id,
+                            logo: strapi.config.get('server.baseUrl', 'defaultValueIfUndefined') + eventserviceproviders[i].logo.url,
+                            firstImage: strapi.config.get('server.baseUrl', 'defaultValueIfUndefined') + eventserviceproviders[i].topImage.url,
+                            name: eventserviceproviders[i].knownName,
+                            city: eventserviceproviders[i].address.city,
+                            rating: eventserviceproviders[i].ratingTotal,
+                            spec: eventserviceproviders[i].smallDescription,
+                            spId: eventserviceproviders[i].id
+                        }
+                        myItems.push(newItem)
+                    }
+                }
+
+            }
+        }
+        console.log(city);
+        return myItems
+    },
+
+    async getListOfRestauration(ctx) {
+        let entities;
+        const { city } = ctx.params;
+        if (ctx.query._q) {
+            entities = await strapi.services.eventserviceprovider.search(ctx.query);
+        } else {
+            entities = await strapi.services.eventserviceprovider.find(ctx.query);
+        }
+        let eventserviceproviders = entities.map(entity => sanitizeEntity(entity, { model: strapi.models.eventserviceprovider }));
+        let myItems = []
+        for (let i = 0; i < eventserviceproviders.length; i++) {
+
+            if ((eventserviceproviders[i].address.city == city) && (eventserviceproviders[i].status)) {
+                for (let d = 0; d < eventserviceproviders[i].showIn.length; d++) {
+                    if (eventserviceproviders[i].showIn[d].serviceName == "event-restauration") {
+                        let newItem = {
+                            id: eventserviceproviders[i].id,
+                            logo: strapi.config.get('server.baseUrl', 'defaultValueIfUndefined') + eventserviceproviders[i].logo.url,
+                            firstImage: strapi.config.get('server.baseUrl', 'defaultValueIfUndefined') + eventserviceproviders[i].topImage.url,
+                            name: eventserviceproviders[i].knownName,
+                            city: eventserviceproviders[i].address.city,
+                            rating: eventserviceproviders[i].ratingTotal,
+                            spec: eventserviceproviders[i].smallDescription,
+                            spId: eventserviceproviders[i].id
+                        }
+                        myItems.push(newItem)
+                    }
+                }
+
+            }
+        }
+        console.log(city);
+        return myItems
+    },
+    async getListOfService(ctx) {
+        let entities;
+        const { city } = ctx.params;
+        if (ctx.query._q) {
+            entities = await strapi.services.eventserviceprovider.search(ctx.query);
+        } else {
+            entities = await strapi.services.eventserviceprovider.find(ctx.query);
+        }
+        let eventserviceproviders = entities.map(entity => sanitizeEntity(entity, { model: strapi.models.eventserviceprovider }));
+        let inCitySP = []
+        let myItems = []
+        for (let i = 0; i < eventserviceproviders.length; i++) {
+
+            if ((eventserviceproviders[i].address.city == city) && (eventserviceproviders[i].status)) {
+                for (let d = 0; d < eventserviceproviders[i].showIn.length; d++) {
+                    if (eventserviceproviders[i].showIn[d].serviceName == "event-service") {
+                        for (let j = 0; j < eventserviceproviders[i].items.length; j++) {
+                            if (eventserviceproviders[i].items[j].status) {
+                                let myspecText = ""
+                                for (let k = 0; k < eventserviceproviders[i].items[j].specification.length; k++) {
+                                    myspecText = myspecText + eventserviceproviders[i].items[j].specification[k].specText + ", "
+                                }
+                                let newItem = {
+                                    id: eventserviceproviders[i].items[j].id,
+                                    logo: strapi.config.get('server.baseUrl', 'defaultValueIfUndefined') + eventserviceproviders[i].logo.url,
+                                    firstImage: strapi.config.get('server.baseUrl', 'defaultValueIfUndefined') + eventserviceproviders[i].items[j].firstImage.url,
+                                    name: eventserviceproviders[i].items[j].name,
+                                    city: eventserviceproviders[i].address.city,
+                                    rating: eventserviceproviders[i].ratingTotal,
+                                    spec: myspecText,
+                                    spId: eventserviceproviders[i].id
+                                }
+                                myItems.push(newItem)
+
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+        console.log(city);
+        return myItems
+    },
     async getListOfPlaces(ctx) {
         let entities;
         const { city } = ctx.params;
@@ -18,28 +131,31 @@ module.exports = {
         let inCitySP = []
         let myItems = []
         for (let i = 0; i < eventserviceproviders.length; i++) {
-            console.log(eventserviceproviders[i].address.city)
-            console.log(city);
-            console.log(eventserviceproviders[i].address.city == city);
-            if ((eventserviceproviders[i].address.city == city) && (eventserviceproviders[i].status)) {
-                for (let j = 0; j < eventserviceproviders[i].items.length; j++) {
-                    if (eventserviceproviders[i].items[j].status) {
-                        let myspecText = ""
-                        for (let k = 0; k < eventserviceproviders[i].items[j].specification.length; k++) {
-                            myspecText = myspecText + eventserviceproviders[i].items[j].specification[k].specText + ", "
-                        }
-                        let newItem = {
-                            id: eventserviceproviders[i].items[j].id,
-                            logo: strapi.config.get('server.baseUrl', 'defaultValueIfUndefined') + eventserviceproviders[i].logo.url,
-                            firstImage: strapi.config.get('server.baseUrl', 'defaultValueIfUndefined') + eventserviceproviders[i].items[j].firstImage.url,
-                            name: eventserviceproviders[i].items[j].name,
-                            city: eventserviceproviders[i].address.city,
-                            rating: eventserviceproviders[i].ratingTotal,
-                            spec: myspecText,
-                            spId: eventserviceproviders[i].id
-                        }
-                        myItems.push(newItem)
 
+            if ((eventserviceproviders[i].address.city == city) && (eventserviceproviders[i].status)) {
+                for (let d = 0; d < eventserviceproviders[i].showIn.length; d++) {
+                    if (eventserviceproviders[i].showIn[d].serviceName == "event-salle") {
+                        for (let j = 0; j < eventserviceproviders[i].items.length; j++) {
+                            if (eventserviceproviders[i].items[j].status) {
+                                let myspecText = ""
+                                for (let k = 0; k < eventserviceproviders[i].items[j].specification.length; k++) {
+                                    myspecText = myspecText + eventserviceproviders[i].items[j].specification[k].specText + ", "
+                                }
+                                let newItem = {
+                                    id: eventserviceproviders[i].items[j].id,
+                                    logo: strapi.config.get('server.baseUrl', 'defaultValueIfUndefined') + eventserviceproviders[i].logo.url,
+                                    firstImage: strapi.config.get('server.baseUrl', 'defaultValueIfUndefined') + eventserviceproviders[i].items[j].firstImage.url,
+                                    name: eventserviceproviders[i].items[j].name,
+                                    city: eventserviceproviders[i].address.city,
+                                    rating: eventserviceproviders[i].ratingTotal,
+                                    spec: myspecText,
+                                    spId: eventserviceproviders[i].id,
+                                    specName:eventserviceproviders[i].knownName
+                                }
+                                myItems.push(newItem)
+
+                            }
+                        }
                     }
                 }
 
