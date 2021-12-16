4788302331c2de5eb934029dc34c4d0d09130312
Mohamed Amine Berroug <ma.berroug@aggrom.com>
2021-12-16 17:52:42 +0100

    finished market and orders

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,26 @@
 'use strict';
-
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+ 
+    async cancelMyOrder(ctx) {
+        const { id } = ctx.params;
+
+        let entity;
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            entity = await strapi.services.order.update({ id }, data, {
+                files,
+            });
+        } else {
+            console.log(ctx.request.body);
+            entity = await strapi.services.order.update({ id }, ctx.request.body);
+        }
+
+        return sanitizeEntity(entity, { model: strapi.models.order });
+    },
+};
