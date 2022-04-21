847698ce075355667ed168c568e976d873a67a58
Mohamed Amine Berroug <ma.berroug@aggrom.com>
2022-04-21 21:30:23 +0000

    changed server

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  baseUrl:"http://localhost:1337",
+  baseUrl:"https://api.binbudget.com",
   cron: { enabled: true },
   admin: {
     auth: {
